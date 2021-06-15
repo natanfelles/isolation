45156c5ba9e7d7f67888577494e1827b1a7cbeb3
Natan Felles <natanfelles@gmail.com>
2021-06-15 16:09:44 -0300

    Data key can be integer

--- src/isolation.php
+++ src/isolation.php
@@ -5,7 +5,7 @@
  * Prevents access to $this/self from required files.
  *
  * @param string $filename The file to be required
- * @param array<string,mixed> $data [optional] Data to be extracted as variables
+ * @param array<int|string,mixed> $data [optional] Data to be extracted as variables
  * present for the required file
  *
  * @return mixed The return of the require
