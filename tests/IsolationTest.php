3ed5077db1fdaea2010f0c399ffe717d73556d20
Natan Felles <natanfelles@gmail.com>
2021-06-28 10:40:32 -0300

    Coding Standard: Add header comments

--- tests/IsolationTest.php
+++ tests/IsolationTest.php
@@ -1,4 +1,13 @@
-<?php namespace Tests\Isolation;
+<?php
+/*
+ * This file is part of The Framework Isolation Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Isolation;
 
 use PHPUnit\Framework\TestCase;
 
